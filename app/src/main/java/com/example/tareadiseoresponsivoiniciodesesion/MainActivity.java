c523c2d3207f62b229acc61c77fb504052c16d0e
JoseLozanoMorales <mozanojosealejandro19029.com>
2025-12-13 17:52:54 -0500

    Adición de los botones de las diferentes formas de inicio de sesión y confirmación de sus pulsaciones.

--- app/src/main/java/com/example/tareadiseoresponsivoiniciodesesion/MainActivity.java
+++ app/src/main/java/com/example/tareadiseoresponsivoiniciodesesion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tareadiseoresponsivoiniciodesesion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Has seleccionado Usuario y contraseña", Toast.LENGTH_SHORT).show());
+
+        button2.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Has seleccionado Huella / Face ID", Toast.LENGTH_SHORT).show());
+
+        button3.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Has seleccionado Pin de 6 dígitos", Toast.LENGTH_SHORT).show());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
